b20c2221e6074d7d618a701f2bdaf1b89c04b0c3
jto <jto@geometrix.lan>
2017-09-11 19:35:01 +0300

    initial

---
+++ app/src/main/java/fi/badgamers/olutta/FullscreenActivity.java
@@ -0,0 +1,32 @@
+package fi.badgamers.olutta;
+
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class FullscreenActivity extends Activity {
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setStatusBarColor(Color.BLACK);
+        mWebView = new WebView(this);
+        mWebView.loadUrl("https://www.badgamers.fi/game/17/o.php");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+
+        this.setContentView(mWebView);
+    }
+}
