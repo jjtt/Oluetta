d9c8d9d928b6bc4719377bac1912f48c5c4f020d
jto <jto@geometrix.lan>
2017-09-12 20:49:20 +0300

    hiding the status bar and navigation buttons

--- app/src/main/java/fi/badgamers/oluetta/FullscreenActivity.java
+++ app/src/main/java/fi/badgamers/oluetta/FullscreenActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -12,11 +13,13 @@ import android.webkit.WebViewClient;
 
 public class FullscreenActivity extends Activity {
     private WebView mWebView;
+    private View mDecorView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        mDecorView = getWindow().getDecorView();
         mWebView = new WebView(this);
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.setBackgroundColor(Color.BLACK);
@@ -31,4 +34,26 @@ public class FullscreenActivity extends Activity {
 
         this.setContentView(mWebView);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    // This snippet hides the system bars.
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+
 }
